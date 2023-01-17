5cc83527d44a0ad4c8d811e94665242ede0fab9f
romstay <romikhoiril@gmail.com>
2023-01-17 19:24:52 +0700

    menambahkan file

--- latihan14.py
+++ latihan14.py
@@ -1,26 +1,7 @@
-# praktikum14
-
-
-~~~bash
-Nama    :Romiza Althafariq Khoiril
-Nim     :312210182
-Kelas   :TI.22.B1
-~~~
-
-
-
-
-## String
-
-### Penjelasan
-
-#### Code
-
-
-- Latihan 1
-
-
-~~~bash
+print("="*12, " STRING ", "="*12)
+print()
+print(f"{'latihan 1':12}")
+print('-'*12)
 txt = 'Hello World'
 print(f"Teks = {txt}")
 # Menghitung jumlah karakter
@@ -43,42 +24,9 @@ print(f"Teks menjadi huruf kecil = {e}")
 # Ganti karakter H dengan karakter J
 f = txt.replace("H","J")
 print(f"Menganti karakter H dengan J = {f}")
-~~~
-
-![image](ss/1.png)
-
-
-
-
-
-
-
-
-
-
-- Latihan 2
-
-
-~~~bash
+print()
+print(f"{'latihan 2':12}")
+print('-'*12)
 umur = 24
 txt = 'Hello, nama saya john, dan umur saya adalah {0} tahun'
 print(txt.format(umur))
-~~~
-
-
-
-### Output
-
-
-
-![image](ss/2.png)
-
-
-
-
-
-
-
-
-
-Terimakasih
